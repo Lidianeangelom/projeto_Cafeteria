bb5859b7a05b983c87133472821a77a448f5aa69
Lidianeangelom <lidiane_angelo@mckinsey.com>
2023-02-23 14:23:03 -0300

    Projeto Cafeteri [Model - 23.02.2023]

---
+++ src/main/java/com/example/cafeteria/model/Estoque.java
@@ -0,0 +1,101 @@
+package com.example.cafeteria.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "estoque")
+public class Estoque {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=2)
+	private String nome;
+	
+	@NotNull
+	@Size(min=4)
+	private Date data_entrada;
+	
+	@NotNull
+	@Size(min=4)
+	private Date data_saida;
+	
+	@NotNull
+	private float faturamento;
+	
+	@NotNull
+	private int estoque_inicial;
+	
+	@NotNull
+	private int estoque_atual;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData_entrada() {
+		return data_entrada;
+	}
+
+	public void setData_entrada(Date data_entrada) {
+		this.data_entrada = data_entrada;
+	}
+
+	public Date getData_saida() {
+		return data_saida;
+	}
+
+	public void setData_saida(Date data_saida) {
+		this.data_saida = data_saida;
+	}
+
+	public float getFaturamento() {
+		return faturamento;
+	}
+
+	public void setFaturamento(float faturamento) {
+		this.faturamento = faturamento;
+	}
+
+	public int getEstoque_inicial() {
+		return estoque_inicial;
+	}
+
+	public void setEstoque_inicial(int estoque_inicial) {
+		this.estoque_inicial = estoque_inicial;
+	}
+
+	public int getEstoque_atual() {
+		return estoque_atual;
+	}
+
+	public void setEstoque_atual(int estoque_atual) {
+		this.estoque_atual = estoque_atual;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/example/cafeteria/model/Produto.java
@@ -0,0 +1,144 @@
+package com.example.cafeteria.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=5)
+	private String tipo;
+	
+	@NotNull
+	@Size(min=5)
+	private String nome;
+	
+	@NotNull
+	@Size(min=1)
+	private String categoria;
+	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	@Size(min=5)
+	private String descricao;
+	
+	@NotNull
+	private float preco;
+	
+	@NotNull
+	@Size(min=4)
+	private Date data_validade;
+	
+	@NotNull
+	@Size(min=5)
+	private String fornecedor;
+	
+	@NotNull
+	@Size(min=5)
+	private String foto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public Date getData_validade() {
+		return data_validade;
+	}
+
+	public void setData_validade(Date data_validade) {
+		this.data_validade = data_validade;
+	}
+
+	public String getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(String fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
