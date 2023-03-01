ea195099992b4b5ab1f124f41817ad5c70859598
Lidianeangelom <lidiane_angelo@mckinsey.com>
2023-03-01 15:58:19 -0300

    Repository - [01.03.2023]

--- src/main/java/com/example/cafeteria/repository/ProdutoRepository.java
+++ src/main/java/com/example/cafeteria/repository/ProdutoRepository.java
@@ -1,16 +1,20 @@
 package com.example.cafeteria.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.example.cafeteria.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-	public List<Produto> findByNome(@Param("nome") String nome);
+	
+	public List<Produto> findAll();
 
-	public Object findAllByNome(String nome);
+	Optional<Produto> findById(Long id);
+	
+	Optional<Produto> findByCategoria(String categoria);
 
-	public Object findByPreco(@Param("preco")Float preco);
+
+
 }
